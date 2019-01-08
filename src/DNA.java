76eb88f8242eb6bb462712d2dac16ad4f3cc80ec
nguacon01 <dungdm6191@gmail.com>
2019-01-08 15:42:08 +0100

    git ignore

--- src/DNA.java
+++ src/DNA.java
@@ -35,7 +35,7 @@ public class DNA extends Sequence {
                 break;
             }
         }
-        System.out.println("codon start at position: "+pos);ad
+        System.out.println("codon start at position: "+pos);
 
         while(pos < this.seqSymbol.length()-3) {
 			if(this.seqSymbol.length() < 3) {
